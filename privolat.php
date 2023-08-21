fc56c4b39b580885f94ccc0116e7209589c2fed7
Tobias <118220231+nejakytobyjas@users.noreply.github.com>
2023-08-21 16:31:36 +0200

    Update privolat.php

--- privolat.php
+++ privolat.php
@@ -3,4 +3,3 @@ header("Access-Control-Allow-Origin: *");
 $fp = fopen('chce.txt', 'c');
 fwrite($fp, 'j');
 fclose($fp);
-echo '<meta http-equiv="refresh" content="0; url=https://www.alik.cz/k/roboti-koutek/" />';
